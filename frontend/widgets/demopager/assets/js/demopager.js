19b52ade3af0e7dc1d2c3ae92568c1c8ded86784
Pritam Das <pritam@asl.aero>
2017-08-09 19:03:15 +0600

    Yii2 Pager Widget Step - 1

--- frontend/widgets/demopager/assets/js/demopager.js
+++ frontend/widgets/demopager/assets/js/demopager.js
@@ -1,3 +1,43 @@
 /**
  * Created by mac on 8/8/17.
  */
+
+var demoPagerWidget = {
+    init: function (options) {
+        if (options.pageSizeParam == 'undefined' || !(options.pageSizeParam)){
+            return;
+        }
+
+        if (options.pageParam == 'undefined' || !(options.pageParam)){
+            return;
+        }
+
+        if (options.url == 'undefined' || !(options.url)){
+            return;
+        }
+
+        this.bindEvent(options.pageSizeParam, options.pageParam, options.url)
+    },
+    bindEvent: function (pageSizeParam, pageParam, url) {
+        $('input[name = "' + pageParam + '"]').on('keydown', function (evt) {
+            if (evt.which == 13){
+                var targetPage = $(this).val();
+
+                var pattern = new RegExp("\(&" + pageParam + "=\)\\d+","gi");
+                var newUrl = url.replace(pattern, "$1"+targetPage);
+                window.location.href = newUrl;
+            }
+
+        });
+        
+        $('select[name="' + pageSizeParam + '"]').on('change',function () {
+            var selectedPageSize = $(this).find('option:selected').val();
+
+            var pattern = new RegExp("\(&" + pageParam + "=\)\\d+","gi");
+            var newUrl = url.replace(pattern, "$1"+targetPage);
+            window.location.href = newUrl;
+            
+        });
+
+    }
+};
