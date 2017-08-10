81bb460bf25805cf4ede1a386ab11ad5f8910e80
Pritam Das <pritam@asl.aero>
2017-08-10 17:24:46 +0600

    Yii2 Pager Widget Step - 2

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -1,12 +1,37 @@
 <?php
-
 namespace frontend\controllers;
-
-class TestController extends \yii\web\Controller
+use Yii;
+use yii\web\Controller;
+use yii\data\ArrayDataProvider;
+class TestController extends Controller
 {
-    public function actionIndex()
+    public function actionTest()
     {
-        return $this->render('index');
+        // Get page size from query strings
+        $page_size = Yii::$app->request->get('per-page');
+        $page_size = isset( $page_size ) ? intval($page_size) : 4;
+        $provider = new ArrayDataProvider([
+            'allModels' => $this->getFakedModels(),
+            'pagination' => [
+                // Should not hard coded
+                'pageSize' => $page_size,
+            ],
+            'sort' => [
+                'attributes' => ['id'],
+            ],
+        ]);
+        return $this->render('test', ['listDataProvider' => $provider]);
     }
-
+    private function getFakedModels()
+    {
+        $rawData = [];
+        for ($i=1; $i < 18; $i++) {
+            $item = [
+                'id' => $i,
+                'title' => 'Title ' . $i,
+            ];
+            $rawData[] = $item;
+        }
+        return $rawData;
+    }
 }
--- frontend/widgets/demopager/assets/js/demopager.js
+++ frontend/widgets/demopager/assets/js/demopager.js
@@ -1,43 +1,51 @@
-/**
- * Created by mac on 8/8/17.
- */
-
 var demoPagerWidget = {
     init: function (options) {
-        if (options.pageSizeParam == 'undefined' || !(options.pageSizeParam)){
+        if (options.pageSizeParam == "undefined" || !(options.pageSizeParam)) {
             return;
         }
 
-        if (options.pageParam == 'undefined' || !(options.pageParam)){
+        if (options.pageParam == "undefined" || !(options.pageParam)) {
             return;
         }
 
-        if (options.url == 'undefined' || !(options.url)){
+        if (options.url == "undefined" || !(options.url)) {
             return;
         }
 
-        this.bindEvent(options.pageSizeParam, options.pageParam, options.url)
+        this.bindEvent(options.pageSizeParam, options.pageParam, options.url);
     },
     bindEvent: function (pageSizeParam, pageParam, url) {
-        $('input[name = "' + pageParam + '"]').on('keydown', function (evt) {
-            if (evt.which == 13){
-                var targetPage = $(this).val();
+        // $('input[name="page"]')
+        $('input[name="' + pageParam + '"]').on('keydown', function (evt) {
+            if (evt.which == 13) {
+                var targetPage =  $(this).val();
+                console.log(targetPage);
 
-                var pattern = new RegExp("\(&" + pageParam + "=\)\\d+","gi");
-                var newUrl = url.replace(pattern, "$1"+targetPage);
-                window.location.href = newUrl;
+                // new RegExp("\(&page=\)\\d+", "gi")
+                // var pattern = new RegExp("\(&" + pageParam + "=\)\\d+", "gi");
+                // var pattern = new RegExp("\(&" + pageParam + "=\)\\d+");
+                // var pattern = new RegExp("\(&page=\)\\d+", "gi");
+                console.log(pattern);
+                console.log("$1"+targetPage);
+                // var newUrl = url.replace(pattern, "$1"+targetPage);
+                // var newUrl = url.replace(pattern, targetPage);
+
+                var pattern = new RegExp("/([?|&]" + pageParam + "=)[^\&]+/");
+                newUrl = url.replace( pattern , '$1' + targetPage);
+                console.log(newUrl);
+
+                // window.location.href = newUrl;
             }
-
         });
-        
-        $('select[name="' + pageSizeParam + '"]').on('change',function () {
+
+        // $('select[name="per-page"]')
+        $('select[name="' + pageSizeParam + '"]').on('change', function () {
             var selectedPageSize = $(this).find('option:selected').val();
 
-            var pattern = new RegExp("\(&" + pageParam + "=\)\\d+","gi");
-            var newUrl = url.replace(pattern, "$1"+targetPage);
+            // new RegExp("\(&per-page=\)\\d+", "gi")
+            var pattern = new RegExp("\(&" + pageSizeParam + "=\)\\d+", "gi");
+            var newUrl = url.replace(pattern, "$1"+selectedPageSize);
             window.location.href = newUrl;
-            
         });
-
     }
 };
